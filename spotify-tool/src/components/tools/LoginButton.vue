cf302940ad6d3bb88531aa2af0caad5ed61696ec
zcAllBru <allan.brunner@zeacold.com>
2024-11-05 21:27:20 +0100

    Part1 of the login implementation

---
+++ spotify-tool/src/components/tools/LoginButton.vue
@@ -0,0 +1,17 @@
+<template>
+    <button @click="login">
+        Login with Spotify
+    </button>
+</template>
+
+<script>
+import { getSpotifyAuthUrl } from '../../services/auth'
+
+export default {
+    methods: {
+        login() {
+            window.location.href = getSpotifyAuthUrl();
+        }
+    }
+}
+</script>
